7fb3a32cf9986d7ef4024f5222334d765ede6826
ibrahimelbanna <proghima54@gmail.com>
2018-12-24 13:39:17 +0200

    Initial commit

--- Blog/app/controllers/comments_controller.rb
+++ Blog/app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+    def create
+        @post = Post.find(params[:post_id])
+        @comments = @post.comments.create(params[:commnet]).permit(:name,:commnet)
+        redirect_to post_path(@post)
+
+    end
+
+    def destroy
+        
+    end
+    
+    
 end
--- Blog/db/schema.rb
+++ Blog/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_24_103840) do
+ActiveRecord::Schema.define(version: 2018_12_24_105036) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
